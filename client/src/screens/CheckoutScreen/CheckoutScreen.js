ba6ac69a790d1dd5b924278b91c663b578feae1a
Allen Chun <allenchun@Allens-MacBook-Pro.local>
2023-01-28 22:10:00 -0500

    test

--- client/src/screens/CheckoutScreen/CheckoutScreen.js
+++ client/src/screens/CheckoutScreen/CheckoutScreen.js
@@ -76,7 +76,7 @@ const submitMessage = async () => {
         
     }
     ws.send(JSON.stringify({table_id: table_id, cart: cart}))
-    ws.close()
+    // ws.close()
     navigation.navigate('Home', {ordered: true})
 }
 
