48c40376625a90f65fb5a70f350804b844618bbc
Allen Chun <allenchun@Allens-MacBook-Pro.local>
2023-02-01 22:11:53 -0500

    test

--- client/src/screens/HomeScreen/HomeScreen.js
+++ client/src/screens/HomeScreen/HomeScreen.js
@@ -18,34 +18,6 @@ LogBox.ignoreLogs(['expo-app-loading is deprecated in favor of expo-splash-scree
 
 
 const HomeScreen = ({route, navigation}) => {
-  const [search, setSearch] = useState('')
-  const { ordered } = route.params
-  // const [restaurantList, setRestaurantList] = useState([])
-  const [index, setIndex] = useState(0)
-
-  const isCarousel = useRef()
-  // const SLIDER_WIDTH = Dimensions.get('window').width + 80
-  // const ITEM_WIDTH = Math.round(SLIDER_WIDTH * 0.7)
-
-  let testRestaurants = [
-    {
-      // id: 0,
-      mainImage: 'https://jackswifefreda.com/wp-content/uploads/2021/06/1_HCH_8344_1280x720-1170x658.jpg',
-      address: '50+Carmine+St+New+York+NY+10014+United+States',
-      foodImage1: 'https://images.otstatic.com/prod1/28948642/4/huge.jpg',
-      foodImage2: 'https://images.otstatic.com/prod1/28948594/5/huge.jpg',
-      foodImage3: 'http://www.prettyinpistachio.com/wp-content/uploads/2013/10/JacksWifeFreda1-72ppi.jpg',
-    },
-    {
-      // id: 1,
-      mainImage: 'https://jackswifefreda.com/wp-content/uploads/2021/06/1_HCH_8344_1280x720-1170x658.jpg',
-      address: '50+Carmine+St+New+York+NY+10014+United+States',
-      foodImage1: 'https://images.otstatic.com/prod1/28948642/4/huge.jpg',
-      foodImage2: 'https://images.otstatic.com/prod1/28948594/5/huge.jpg',
-      foodImage3: 'http://www.prettyinpistachio.com/wp-content/uploads/2013/10/JacksWifeFreda1-72ppi.jpg',
-    }
-  ]
-
   const [restaurantList, setRestaurantList] = useState([])
 
   // Fetch Call
@@ -65,19 +37,8 @@ const HomeScreen = ({route, navigation}) => {
     console.log(restaurantList)
   }, [])
 
-  // const [fontsLoaded] = useFonts({
-  //   'Jost': require('../../../assets/fonts/Jost-Regular.ttf'),
-  // });
-
-  // const fetchFonts = async () =>
-  //   await Font.loadAsync({
-  //     'Jost': require('../../../assets/fonts/Jost-Regular.ttf'),
-  // });
-
   return (
     <View style = {styles.container}>
-
-      <View style={styles.spacerFlex}></View>
 
       <View style={styles.titleContainer}>
         <Text style = {styles.title}>
@@ -102,8 +63,6 @@ const HomeScreen = ({route, navigation}) => {
         <View style={{marginTop: Dimensions.get('window').height * 0.05}}>
           <CarouselCards data={restaurantList}/>
         </View>
-        {/* <RestaurantCard restaurant = {testRestaurants[0]}/> */}
-      
       </View>
     </View>
   )
@@ -114,7 +73,6 @@ const styles = StyleSheet.create({
         flex: 1, 
         backgroundColor: '#f6f5f5',
     },
-
     
     titleContainer: {
       flex: 2,
@@ -148,36 +106,6 @@ const styles = StyleSheet.create({
       justifyContent: 'center',
       alignItems: 'center'
     },
-
-    
-
-
-
-    card: {
-      // width: Dimensions.get('window').width * 0.3,
-      // height: Dimensions.get('window').height * 0.1,
-      width: 100,
-      height: 100,
-      // shadowColor: '#171717',
-      // // shadowOffset: {width: -2, height: 4},
-      // shadowOpacity: 0.3,
-      // shadowRadius: 4,
-      borderColor: 'black',
-      borderWidth: 10,
-      backgroundColor: 'red'
-  },
-  restaurantName: {
-      fontWeight: 'bold',
-      fontColor: 'red'
-  },
-  restaurantImagesContainer: {
-      flexDirection: 'row'
-  },
-  restaurantImages: {
-
-  }
-
-
 })
 
 export default HomeScreen
