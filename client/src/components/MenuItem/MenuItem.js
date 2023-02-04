eddbe22e42eccadd0a677b1edc5214c294b0faa2
Allen Chun <allenchun@Allens-MacBook-Pro.local>
2023-02-03 22:55:29 -0500

    touched up menu screen

--- client/src/components/MenuItem/MenuItem.js
+++ client/src/components/MenuItem/MenuItem.js
@@ -1,39 +1,49 @@
-import { StyleSheet, Text, View, Pressable, TouchableOpacity } from 'react-native'
+import { StyleSheet, Text, View, TouchableHighlight, Dimensions, Image } from 'react-native'
 import React from 'react'
 import NumberFormat from 'react-number-format'
 
 const MenuItem = ({navigation, item, name, price, description, subtotal, restaurant_id, table_id, isOrdering, restaurant_name}) => {
   return (
-    <TouchableOpacity
-        style = {styles.container}
-        onPress = {() => navigation.navigate('Item', {item: item, name: name, description: description, 
-                                                      price: price,
+    <TouchableHighlight
+    style={{width: '100%', alignItems: 'center'}}
+        underlayColor='#E5EFE3'
+        onPress = {() => navigation.navigate('Item', {item: item,
                                                       subtotal: subtotal, restaurant_id : restaurant_id, restaurant_name: restaurant_name, table_id: table_id, isOrdering: isOrdering})}>
             
-      <Text style = {styles.itemName}>{name}</Text>
+          <View style={styles.container}>
+            <View style={{width: '75%'}}>
+              <Text style = {styles.itemName}>{name}</Text>
 
-      <NumberFormat
-            value = {price}
-            displayType = "text"
-            thousandSeparator={true}
-            prefix = "$"
-            decimalScale={2}
-            fixedDecimalScale = {true}
-            renderText={(value) => <Text style = {styles.itemPrice}>{value}</Text>}>
-        </NumberFormat>
+              <NumberFormat
+                    value = {price}
+                    displayType = "text"
+                    thousandSeparator={true}
+                    prefix = "$"
+                    decimalScale={2}
+                    fixedDecimalScale = {true}
+                    renderText={(value) => <Text style = {styles.itemPrice}>{value}</Text>}>
+                </NumberFormat>
 
-      <Text style = {styles.itemDescription}>
-        {description}
-      </Text>
+              <Text style = {styles.itemDescription}>
+                {description}
+              </Text>
+            </View>
+            <Image style={styles.image} source={{uri: 'https://www.elmundoeats.com/wp-content/uploads/2021/02/FP-Quick-30-minutes-chicken-ramen.jpg'}}/>
+
+          </View>
       
-    </TouchableOpacity>
+    </TouchableHighlight>
   )
 }
 
 const styles = StyleSheet.create({
     container: {
-        marginVertical: 15,
-        marginLeft: 10
+        // height: Dimensions.get('window').height * 0.11,
+        width: '90%',
+        paddingVertical: Dimensions.get('window').height * 0.021,
+        flexDirection: 'row',
+        borderBottomWidth: 0.5,
+        borderColor: '#E5E5E5'
     },
 
     itemName: {
@@ -46,7 +56,13 @@ const styles = StyleSheet.create({
 
     itemDescription: {
         color: '#7C7878',
-        marginTop: 5
+        marginTop: 5,
+        width: '95%',
+    },
+    image: {
+      height: 90,
+      width: '25%',
+      borderRadius: 10,
     }
 })
 
