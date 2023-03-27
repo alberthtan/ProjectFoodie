b4d2126cffd94f8bd6989abb65d0eaf15f7a6e9f
Albert Tan <alberttan@Alberts-MBP.wireless.yale.internal>
2023-03-27 00:20:02 -0400

    live mode

--- client/src/Stripe/Stripe.js
+++ client/src/Stripe/Stripe.js
@@ -51,7 +51,7 @@ const Stripe = ({navigation}) => {
 
   return (
     <StripeProvider
-        publishableKey="pk_test_51Mg3eiFsXLeRVzOVO5KIPsOT13pU8MbJss8HLpEeUmyEkqKazhvDwFRoKbq8qIMzgZ8O2ngX793aPY1UqywLPdXa00d0M1qARt"
+        publishableKey="pk_live_51Mg3eiFsXLeRVzOVijgZAOahtncfYOPii0G3aA4W5QMAeQb4dw0Pl1FazfIwZzfYCjNjbU7iDydGVVC8DLCJf5O800lrWkkMUq"
         // merchantIdentifier='merchant.identifier'
     >
         <SafeAreaView>
